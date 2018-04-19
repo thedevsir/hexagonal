0e897b03a4aa281e848a452e2a80df1ba1994ada
Mohammad Hasani <atretoname@gmail.com>
2018-04-19 16:06:18 +0430

    feat: register service worker and add manifest file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { App } from './app';
+import { register } from './sw';
 import './index.scss';
 
 render(<App />, document.getElementById('root') as HTMLElement);
+register();
