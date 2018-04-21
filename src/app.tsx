b8cb73a430dc8cb9972d83ec3e80dc380cfca6c4
Mohammad Hasani <atretoname@gmail.com>
2018-04-21 20:42:03 +0430

    feat: support `React.StrictMode` to detect issues with React lifecycle

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, StrictMode } from 'react';
 import { hot } from 'react-hot-loader';
 
 import styles from './app.module.scss';
@@ -8,15 +8,17 @@ import logo from './logo.svg';
 export class App extends Component {
   render() {
     return (
-      <div className={styles.app}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h1 className={styles.title}>Welcome to React</h1>
-        </header>
-        <p className={styles.intro}>
-          To get started, edit <code>src/app.tsx</code> and save to reload.
-        </p>
-      </div>
+      <StrictMode>
+        <div className={styles.app}>
+          <header className={styles.header}>
+            <img src={logo} className={styles.logo} alt="logo" />
+            <h1 className={styles.title}>Welcome to React</h1>
+          </header>
+          <p className={styles.intro}>
+            To get started, edit <code>src/app.tsx</code> and save to reload.
+          </p>
+        </div>
+      </StrictMode>
     );
   }
 }
