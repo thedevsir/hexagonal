51e7cfedd7728c7f8250e4f8aca9e7f8fb015050
Mohammad Hasani <atretoname@gmail.com>
2018-05-21 22:47:13 +0430

    feat: remodel backdrop template and fix displayName

--- src/app/shared/components/backdrop/backdrop.tsx
+++ src/app/shared/components/backdrop/backdrop.tsx
@@ -1,35 +1,37 @@
 import React, { ComponentType, MouseEventHandler, SFC } from 'react';
 
-import styles from './backdrop.module.scss';
+import { getHocDisplayName } from 'utils';
 
-export type BackdropProps = {
+import styles from './backdrop.module.scss';
+import { Omit } from 'react-router';
+
+export type InjectedBackdropProps = {};
+
+export type OwnBackdropProps = {
   show: boolean;
   onBackdropClick?: MouseEventHandler<HTMLElement>;
 };
 
-export const backdrop = <P extends object>(
-  WrappedComponent: ComponentType<P>
-  // ignored because of typescript issue on generic types spread operation
-  // https://github.com/Microsoft/TypeScript/issues/10727
-  // @ts-ignore
-): SFC<P & BackdropProps> => ({ show, onBackdropClick, ...rest }) => {
-  let backdropElement: HTMLElement;
+export const backdrop = <P extends {}>(WrappedComponent: ComponentType<P>) => {
+  type BackdropProps = Omit<P, keyof InjectedBackdropProps> & OwnBackdropProps;
 
-  const handleBackdropClick: MouseEventHandler<HTMLElement> = event => {
-    if (event.target !== backdropElement) {
-      return;
-    }
-
-    onBackdropClick && onBackdropClick(event); // tslint:disable-line:no-unused-expression
+  const Backdrop: SFC<BackdropProps> = ({
+    show,
+    onBackdropClick,
+    // ignored because of typescript issue on generic types spread operation
+    // https://github.com/Microsoft/TypeScript/issues/10727
+    // @ts-ignore
+    ...props
+  }) => {
+    return show ? (
+      <div className={styles.container}>
+        <div className={styles.backdrop} onClick={onBackdropClick} />
+        <WrappedComponent {...props} />
+      </div>
+    ) : null;
   };
 
-  return show ? (
-    <div
-      className={styles.backdrop}
-      ref={element => (backdropElement = element!)}
-      onClick={handleBackdropClick}
-    >
-      <WrappedComponent {...rest} />
-    </div>
-  ) : null;
+  Backdrop.displayName = getHocDisplayName(Backdrop, WrappedComponent);
+
+  return Backdrop;
 };
