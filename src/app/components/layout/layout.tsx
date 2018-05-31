1ae9de3a7be635582d5184a2ba86cb54abb9162d
Mohammad Hasani <atretoname@gmail.com>
2018-05-31 23:48:59 +0430

    feat: add login modal to layout

--- src/app/components/layout/layout.tsx
+++ src/app/components/layout/layout.tsx
@@ -1,10 +1,9 @@
 import React, { PureComponent } from 'react';
-import {
-  withRouter as router,
-  Switch,
-  RouteComponentProps,
-} from 'react-router-dom';
+import { withRouter as router, Switch, RouteComponentProps } from 'react-router-dom';
 import { Location } from 'history';
+
+import { GuestRoute } from 'app/shared';
+import { Login, LoginModal } from 'app/screens';
 
 import { Header } from './header';
 import { SideNav } from './side-nav';
@@ -12,79 +11,78 @@ import { BackdropSwitch } from './backdrop-switch';
 
 import styles from './layout.module.scss';
 
-export type LayoutProps = RouteComponentProps<{}>;
+export type LayoutProps = RouteComponentProps<any>;
 
 export type LayoutState = {
-  showSideNav: boolean;
-  prevLocation?: Location;
+    showSideNav: boolean;
+    prevLocation?: Location;
 };
 
 export const Layout = router(
-  class extends PureComponent<LayoutProps, LayoutState> {
-    static displayName = 'Layout';
+    class extends PureComponent<LayoutProps, LayoutState> {
+        static displayName = 'Layout';
 
-    state: LayoutState = {
-      showSideNav: false,
-    };
+        state: LayoutState = {
+            showSideNav: false,
+        };
 
-    sideNavToggle = () => (
-      <button
-        className={styles.sideNavToggle}
-        onClick={() =>
-          this.setState({
-            showSideNav: true,
-          })
-        }
-      >
-        <span className={styles.sideNavToggleMiddleLine} />
-      </button>
-    );
+        sideNavToggle = () => (
+            <button
+                className={styles.sideNavToggle}
+                onClick={() =>
+                    this.setState({
+                        showSideNav: true,
+                    })
+                }
+            >
+                <span className={styles.sideNavToggleMiddleLine} />
+            </button>
+        );
 
-    static getDerivedStateFromProps(
-      { location }: LayoutProps,
-      { prevLocation }: LayoutState
-    ): Partial<LayoutState> | null {
-      if (prevLocation && location.state && location.state.backdrop) {
-        return null;
-      }
-
-      return {
-        prevLocation: location,
-      };
-    }
-
-    render() {
-      const { location, history } = this.props;
-      const { showSideNav, prevLocation } = this.state;
-
-      return (
-        <>
-          <Header sideNavToggle={this.sideNavToggle} />
-          <main>
-            <Switch location={prevLocation ? prevLocation : location} />
-          </main>
-          <SideNav
-            show={showSideNav}
-            onBackdropClick={() => this.setState({ showSideNav: false })}
-          />
-          <BackdropSwitch
-            show={
-              location.state &&
-              location.state.backdrop &&
-              prevLocation !== location
+        static getDerivedStateFromProps({ location }: LayoutProps, { prevLocation }: LayoutState): Partial<LayoutState> | null {
+            if (prevLocation && location.state && location.state.backdrop) {
+                return null;
             }
-            onBackdropClick={() =>
-              history.push({
+
+            return {
+                prevLocation: location,
+            };
+        }
+
+        render() {
+            const { location } = this.props;
+            const { showSideNav, prevLocation } = this.state;
+
+            return (
+                <>
+                    <Header sideNavToggle={this.sideNavToggle} />
+                    <main>
+                        <Switch location={prevLocation ? prevLocation : location}>
+                            <GuestRoute path="/login" component={Login} />
+                        </Switch>
+                    </main>
+                    <SideNav show={showSideNav} onBackdropClick={() => this.setState({ showSideNav: false })} />
+                    <BackdropSwitch
+                        show={location.state && location.state.backdrop && prevLocation !== location}
+                        onBackdropClick={this._handleModalClose}
+                    >
+                        <GuestRoute path="/login" render={props => <LoginModal {...props} onRequestClose={this._handleModalClose} />} />
+                    </BackdropSwitch>
+                </>
+            );
+        }
+
+        private _handleModalClose = () => {
+            const { prevLocation } = this.state;
+
+            this.props.history.push({
                 pathname: '/',
                 ...prevLocation,
                 state: {
-                  backdrop: false,
+                    ...(prevLocation && prevLocation.state),
+                    backdrop: false,
                 },
-              })
-            }
-          />
-        </>
-      );
+            });
+        };
     }
-  }
 );
