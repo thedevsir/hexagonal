0df2efcbaa289122bdbe08e623245cda9b75970d
Mohammad Hasani <atretoname@gmail.com>
2018-05-31 19:58:50 +0430

    feat: add routing and auth conditions to navigation

--- src/app/components/layout/nav.tsx
+++ src/app/components/layout/nav.tsx
@@ -1,24 +1,27 @@
-import React, { SFC, DetailedHTMLProps, HTMLAttributes } from 'react';
+import React, { SFC } from 'react';
+import { withRouter as router, RouteComponentProps } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 import classNames from 'classnames';
 
-import { Chip } from 'app/shared';
+import { Chip, BackdropLink, AUTH, Auth } from 'app/shared';
 
 import styles from './nav.module.scss';
 
-export type NavProps = DetailedHTMLProps<
-  HTMLAttributes<HTMLElement>,
-  HTMLElement
->;
+export type NavProps = { auth?: Auth; className?: string } & RouteComponentProps<any>;
 
-export const Nav: SFC<NavProps> = ({ className, ...rest }) => (
-  <nav className={classNames(styles.nav, className)} {...rest}>
-    <Chip>
-      <img
-        src="https://avatars0.githubusercontent.com/u/1410429?s=460&v=4"
-        alt="avatar"
-        className={styles.avatar}
-      />
-      <span>freshmanlimited</span>
-    </Chip>
-  </nav>
+export const Nav = router(
+    inject(AUTH)(
+        observer<SFC<NavProps>>(({ auth, className, history, location }) => (
+            <nav className={classNames(styles.nav, className)}>
+                {auth!.isAuthenticated ? (
+                    <Chip>
+                        <img src="https://avatars0.githubusercontent.com/u/1410429?s=460&v=4" alt="avatar" className={styles.avatar} />
+                        <span>{auth!.token!.payload!.user.username}</span>
+                    </Chip>
+                ) : (
+                    (location.pathname !== '/login' || history.action === 'PUSH') && <BackdropLink to="/login">LOGIN</BackdropLink>
+                )}
+            </nav>
+        ))
+    )
 );
