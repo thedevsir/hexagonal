f88230ab8ccd858c7f2c14c815217ae0395d6a52
Mohammad Hasani <atretoname@gmail.com>
2018-06-01 20:53:15 +0430

    feat: pass axios config in auth service methods

--- src/app/shared/services/auth-api.ts
+++ src/app/shared/services/auth-api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import { transformResponseToRawToken } from 'utils';
 
@@ -10,11 +10,12 @@ export type ResetPasswordReqData = { key: string; password: string };
 export type VerifyReqData = { key: string };
 
 export const AuthApi = {
-    forgotPassword: (data: ForgotPasswordReqData) => axios.post('/login/forgot', data),
-    login: async (data: LoginReqData) => transformResponseToRawToken(await axios.post('/login', data)),
-    logout: () => axios.delete('/logout'),
-    register: async (data: RegisterReqData) => transformResponseToRawToken(await axios.post('/signup', data)),
-    resendEmail: (data: ResendEmailReqData) => axios.post('/signup/resend-email', data),
-    resetPassword: (data: ResetPasswordReqData) => axios.post('/login/reset', data),
-    verify: (data: VerifyReqData) => axios.post('/signup/verify', data),
+    forgotPassword: (data: ForgotPasswordReqData, config?: AxiosRequestConfig) => axios.post('/login/forgot', data, config),
+    login: async (data: LoginReqData, config?: AxiosRequestConfig) => transformResponseToRawToken(await axios.post('/login', data, config)),
+    logout: (config?: AxiosRequestConfig) => axios.delete('/logout', config),
+    register: async (data: RegisterReqData, config?: AxiosRequestConfig) =>
+        transformResponseToRawToken(await axios.post('/signup', data, config)),
+    resendEmail: (data: ResendEmailReqData, config?: AxiosRequestConfig) => axios.post('/signup/resend-email', data, config),
+    resetPassword: (data: ResetPasswordReqData, config?: AxiosRequestConfig) => axios.post('/login/reset', data, config),
+    verify: (data: VerifyReqData, config?: AxiosRequestConfig) => axios.post('/signup/verify', data, config),
 };
