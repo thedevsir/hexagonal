178edb61194580f7ed1998ea7213d1d5f1a28447
Mohammad Hasani <atretoname@gmail.com>
2018-06-02 01:44:19 +0430

    feat: add success modifier to `Status` component

--- src/app/screens/auth/shared/components/form/status.tsx
+++ src/app/screens/auth/shared/components/form/status.tsx
@@ -3,6 +3,18 @@ import classNames from 'classnames';
 
 import styles from './status.module.scss';
 
-export type StatusProps = DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>;
+export enum StatusModifier {
+    success,
+    error,
+}
 
-export const Status: SFC<StatusProps> = ({ className, ...rest }) => <p className={classNames(styles.status, className)} {...rest} />;
+export type StatusProps = {
+    message?: string;
+    modifier?: StatusModifier;
+} & DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>;
+
+export const Status: SFC<StatusProps> = ({ modifier = StatusModifier.error, className, message, children, ...rest }) => (
+    <p className={classNames(styles.status, modifier === StatusModifier.success ? styles.success : styles.error, className)} {...rest}>
+        {message || children}
+    </p>
+);
