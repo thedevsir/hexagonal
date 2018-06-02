c59c1d885835ea80b90bd09638434bd6c906cdd8
Mohammad Hasani <atretoname@gmail.com>
2018-06-02 23:34:29 +0430

    feat: add logout button to `Nav` component

--- src/app/components/layout/nav.tsx
+++ src/app/components/layout/nav.tsx
@@ -3,7 +3,7 @@ import { withRouter as router, RouteComponentProps } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import classNames from 'classnames';
 
-import { Chip, BackdropLink, AUTH, Auth } from 'app/shared';
+import { AUTH, Auth, Chip, Button, BackdropLink } from 'app/shared';
 
 import styles from './nav.module.scss';
 
@@ -14,10 +14,13 @@ export const Nav = router(
         observer<SFC<NavProps>>(({ auth, className, history, location }) => (
             <nav className={classNames(styles.nav, className)}>
                 {auth!.isAuthenticated ? (
-                    <Chip>
-                        <img src="https://avatars0.githubusercontent.com/u/1410429?s=460&v=4" alt="avatar" className={styles.avatar} />
-                        <span>{auth!.token!.payload!.user.username}</span>
-                    </Chip>
+                    <>
+                        <Chip>
+                            <img src="https://avatars0.githubusercontent.com/u/1410429?s=460&v=4" alt="avatar" className={styles.avatar} />
+                            <span>{auth!.token!.payload!.user.username}</span>
+                        </Chip>
+                        <Button onClick={() => auth!.logout()}>LOG OUT</Button>
+                    </>
                 ) : (
                     (location.pathname !== '/login' || history.action === 'PUSH') && <BackdropLink to="/login">LOGIN</BackdropLink>
                 )}
