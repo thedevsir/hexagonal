d43c39c24f53c7e73c5fe36a8240a1202d2f4e56
Mohammad Hasani <atretoname@gmail.com>
2018-07-15 18:40:19 +0430

    feat: add passowrd management page

--- src/app/screens/settings/screens/profile/shared/services/users-api.ts
+++ src/app/screens/settings/screens/profile/shared/services/users-api.ts
@@ -1,7 +1,10 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 export type UpdateMyAccountReqData = { username: string; email: string };
+export type UpdateMyPasswordReqData = { password: string };
 
 export const UsersApi = {
     updateMyAccount: (data: UpdateMyAccountReqData, config?: AxiosRequestConfig) => axios.put('/users/my', data, config),
+
+    updateMyPassword: (data: UpdateMyPasswordReqData, config?: AxiosRequestConfig) => axios.put('/users/my/password', data, config),
 };
